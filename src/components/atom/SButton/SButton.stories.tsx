df9ef5696fd4b3a5e3e2460d165f6e0bf01aaa7a
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2021-12-23 00:25:46 +0100

    init work

---
+++ src/components/atom/SButton/SButton.stories.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import SButton from "./SButton";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "S/Button",
+  component: SButton,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof SButton>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof SButton> = (args) => (
+  <SButton {...args} />
+);
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: "Button",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: "Button",
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: "large",
+  label: "Button",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: "small",
+  label: "Button",
+};
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./components/atom/SButton";
+// export * from "./stories/Button";
+// export * from "./stories/Header";
+// export * from "./stories/Page";

b38c5f3761f7a4e9ed41c45440e5fd7c7bb469b2
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2022-01-14 10:33:50 +0100

    test: cleanup tests
    remove unused code from test

--- .hygen/new/component/component.tsx.ejs.t
+++ .hygen/new/component/component.tsx.ejs.t
@@ -1,14 +1,15 @@
 ---
 to: <%= absPath %>/<%= component_name %>.tsx
 ---
+import { Box } from "@mui/material";
 import { FC } from "react";
 import "./<%= component_name %>.scss";
 import { I<%= component_name %>Props } from "./types";
 
 export const <%= component_name %>: FC<I<%= component_name %>Props> = ({ dummy }) => {
   return (
-    <div className="<%= component_name %>__container">
+    <Box className="<%= component_name %>__container">
       <h1>{dummy}</h1>
-    </div>
+    </Box>
   );
 };
--- src/components/atoms/SLogo/SLogo.spec.tsx
+++ src/components/atoms/SLogo/SLogo.spec.tsx
@@ -1,20 +1,6 @@
-// import { render } from "@testing-library/react";
 import { SvgLogoSygnet } from "s-brand";
 import { shallow } from "enzyme";
 import { SLogo } from ".";
-
-// describe("Components", () => {
-//   describe("atoms", () => {
-//     describe("SLogo", () => {
-//       it("should render without errors", () => {
-//         // const view = render(<SLogo logoSrc={<SvgLogoSygnet />} />);
-//         // expect(view).toBeTruthy();
-//       });
-//     });
-//   });
-// });
-
-// import { SLogo } from "./SLogo";
 
 const wrap = (props) => shallow(<SLogo {...props} />).dive();
 
@@ -23,7 +9,6 @@ describe("Components", () => {
     describe("SLogo", () => {
       it("renders children when passed in", () => {
         const wrapper = wrap({ logoSrc: <SvgLogoSygnet /> });
-        console.log(wrapper.debug());
         expect(wrapper.find(".SLogo__container")).toHaveLength(1);
         expect(wrapper.find(SvgLogoSygnet)).toHaveLength(1);
         expect(wrapper.find(SvgLogoSygnet).dive().find("svg")).toHaveLength(1);

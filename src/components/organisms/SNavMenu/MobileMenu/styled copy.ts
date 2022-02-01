ef214e1d8117fb93b43d0e0c647bcb371b0cd297
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2022-02-02 00:09:08 +0100

    feat(component): navMenu component

--- src/components/organisms/SNavMenu/MobileMenu/styled copy.ts
+++ src/components/organisms/SNavMenu/MobileMenu/styled copy.ts
@@ -1,23 +1,37 @@
 import { Box } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import styled from "@emotion/styled";
+import { MobileMenu } from "./MobileMenu";
 
 const theme = createTheme();
 
+export const StyledMobileMenu = styled(MobileMenu)({
+  position: "absolute",
+  top: 0,
+  left: 0,
+});
+
 export const Container = styled(Box)({
-  position: "relative",
-  backgroundColor: theme.palette.background.paper,
   display: "flex",
   width: "100%",
 });
 
 export const LogoContainer = styled(Box)({
+  padding: theme.spacing(2),
   display: "flex",
   height: "100%",
-  width: "100%",
   justifyContent: "flex-start",
   svg: {
     height: "100%",
     width: "auto",
   },
+});
+
+export const NavContainer = styled(Box)({
+  display: "flex",
+  padding: theme.spacing(2),
+  alignItems: "center",
+  marginLeft: "auto",
+  width: "100%",
+  justifyContent: "flex-end",
 });

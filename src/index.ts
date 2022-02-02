ecab263c882021b085bb6b935d9df2d48ed1bf60
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2022-02-02 01:02:05 +0100

    Revert "feat(component): navMenu component"
    This reverts commit ef214e1d8117fb93b43d0e0c647bcb371b0cd297.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export * from "./components/atoms/SLogo";
 export * from "./components/atoms/SLink";
 export * from "./components/molecules/SIconLink";
-export * from "./components/organisms/SNavMenu";
